e9134247bda5f7b911f7ef8979f1b6ee1edaaed2
Colosonoso <36230695+Colosonoso@users.noreply.github.com>
2018-05-04 11:33:06 -0300

    Add files via upload

---
+++ Validacion de una letra, numero y espacio/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+
+   /*
+
+   char romanos[]={'I','V','X','C','M'};
+
+    int isalpha(char) esto me dice si es una letra o no
+
+   int isDigit(char) esto me dice si es digito o no si es un numero
+
+   int isSpace(char) esto me dice si tiene espacio o no */
+
+  /* printf("Ingrese un dato");
+
+   gris(auxstring);
+
+   get=ValidoStringgenero(auxstring)
+
+     if(get==1)
+     {
+         edad=atoi(auxstring);
+
+     }
+
+     else
+     {
+
+     }
+    return 0;*/
+}
